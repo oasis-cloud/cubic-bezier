57adb1a85862892dce2a5f22a89604fc470fb0fd
wangbei6 <wangbei6@jd.com>
2020-09-02 17:38:50 +0800

    feat:界面

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <div className="output">
+          <span>cubic-bezier(0.5, 0.5, 0.5, 0.5)</span>
+          <em className="btn">复制</em>
+        </div>
+        <div className="preset">
+          <div className="title">预设</div>
+          <ul className="preset-ul">
+            <li className="preset-item"></li>
+            <li className="preset-item"></li>
+            <li className="preset-item"></li>
+            <li className="preset-item"></li>
+            <li className="preset-item"></li>
+            <li className="preset-item"></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="stage">
+        <div className="title">
+          <span>控制器</span>
+          <em className="btn">重置</em>
+        </div>
+        <div className="canvas">
+          <span className="contr c1"></span>
+          <span className="contr c2"></span>
+          <span className="contr c3"></span>
+          <span className="contr c4"></span>
+          <canvas width="800" height="450" ></canvas>
+        </div>
+      </div>
+
     </div>
   );
 }
