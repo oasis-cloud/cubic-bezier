802c717d5ea8d14e1c66110295e7396c711816e4
wanbei <liuluopingyue@sina.com>
2020-09-13 22:57:10 +0800

    feat：逻辑增加

--- src/App.js
+++ src/App.js
@@ -1,77 +1,145 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
+    this.selectedControl = undefined
+    this.canvasRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.$canvas = document.querySelector('#canvas')
+    this.$obj = {
+      c1: document.querySelector('.c1'),
+      c2: document.querySelector('.c2'),
+      c3: document.querySelector('.c3'),
+      c4: document.querySelector('.c4'),
     }
-    componentDidMount() {
-        const $canvas = document.querySelector("#canvas")
-        const $c2 = document.querySelector(".c2")
-        const $c4 = document.querySelector(".c4")
+    this.ctx = this.$canvas.getContext('2d')
+    this.drawSegment()
+    this.drawBezierCurveTo()
+    this.$canvas.addEventListener(
+      'mousemove',
+      e => {
+        let { pageX, pageY } = e
+        const { offsetTop, offsetLeft } = this.canvasRef.current
+        // 因为背景剧中了，所以需要减去背景缩进值来修正鼠标点，绘制控制点则需要加上缩进值，因为控制点相对与canvas定位
+        const DValX = e.target.offsetWidth / 4
+        const DValY = e.target.offsetHeight / 4
+        pageX = pageX - offsetLeft - DValX
+        pageY = pageY - offsetTop - DValY
 
-        const $c2w = $c2.offsetWidth
-        const $c2h = $c2.offsetHeight
+        if (this.selectedControl) {
+          this.$obj[this.selectedControl].style = `left:${pageX + DValX}px;top:${pageY + DValY}px`
+          this.ctx.clearRect(0, 0, 500, 500)
+          this.drawSegment()
+          this.drawBezierCurveTo()
+        }
+      },
+      false
+    )
 
-        const ctx = $canvas.getContext('2d');
+    document.querySelector('.canvas').addEventListener(
+      'mousedown',
+      e => {
+        e.preventDefault()
+        this.selectedControl = e.target.getAttribute('contr-type')
+      },
+      false
+    )
 
-        ctx.beginPath();
-        ctx.moveTo($c2.offsetLeft + $c2w/2, $c2.offsetTop + $c2h/2);
-        ctx.lineTo($c4.offsetLeft + $c2w/2, $c4.offsetTop + $c2h/2);
-        ctx.fill();
-        ctx.lineWidth = 3;//线条的宽度
-        ctx.strokeStyle = "rgba(0,0,0,.1)";//线条的颜色
-        ctx.stroke();
+    document.querySelector('.canvas').addEventListener(
+      'mouseup',
+      e => {
+        e.preventDefault()
+        this.selectedControl = undefined
+      },
+      false
+    )
+  }
 
-        // bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+  drawBezierCurveTo() {
+    const { $obj, ctx } = this
+    const { offsetWidth, offsetHeight } = $obj.c1
 
-        // 设置监听
-        const $c1 = document.querySelector(".c1")
-        const $c3 = document.querySelector(".c3")
+    ctx.beginPath()
+    ctx.moveTo($obj.c2.offsetLeft + offsetWidth / 2, $obj.c2.offsetTop + offsetHeight / 2)
+    ctx.bezierCurveTo(
+      $obj.c1.offsetLeft + offsetWidth / 2,
+      $obj.c1.offsetTop + offsetHeight / 2,
+      $obj.c3.offsetLeft + offsetWidth / 2,
+      $obj.c3.offsetTop + offsetHeight / 2,
+      $obj.c4.offsetLeft + offsetWidth / 2,
+      $obj.c4.offsetTop + offsetHeight / 2
+    )
+    ctx.lineWidth = 5 //线条的宽度
+    ctx.strokeStyle = '#ff89ac' //线条的颜色
+    ctx.stroke()
+    // todo 这里需要根据坐标计算贝塞尔值
+  }
 
-        $canvas.addEventListener("mousemove",(e) => {
-            console.log(e.target)
-        }, false)
-    }
+  drawSegment() {
+    const { $obj, ctx } = this
+    const { offsetWidth, offsetHeight } = $obj.c1
 
-    render() {
-        return (
-            <div className="App">
-                <div className="sidebar">
-                    <div className="output">
-                        <span>cubic-bezier(0.5, 0.5, 0.5, 0.5)</span>
-                        <em className="btn">复制</em>
-                    </div>
-                    <div className="preset">
-                        <div className="title">预设</div>
-                        <ul className="preset-ul">
-                            <li className="preset-item"></li>
-                            <li className="preset-item"></li>
-                            <li className="preset-item"></li>
-                            <li className="preset-item"></li>
-                            <li className="preset-item"></li>
-                            <li className="preset-item"></li>
-                        </ul>
-                    </div>
-                </div>
+    ctx.beginPath()
+    ctx.moveTo($obj.c2.offsetLeft + offsetWidth / 2, $obj.c2.offsetTop + offsetHeight / 2)
+    ctx.lineTo($obj.c1.offsetLeft + offsetWidth / 2, $obj.c1.offsetTop + offsetHeight / 2)
+    ctx.lineWidth = 3 //线条的宽度
+    ctx.strokeStyle = '#FCE811' //线条的颜色
+    ctx.stroke()
+    // 画另外一条控制线
+    ctx.beginPath()
+    ctx.moveTo($obj.c3.offsetLeft + offsetWidth / 2, $obj.c3.offsetTop + offsetHeight / 2)
+    ctx.lineTo($obj.c4.offsetLeft + offsetWidth / 2, $obj.c4.offsetTop + offsetHeight / 2)
+    ctx.lineWidth = 3 //线条的宽度
+    ctx.strokeStyle = '#FCE811' //线条的颜色
+    ctx.stroke()
+  }
 
-                <div className="stage">
-                    <div className="title">
-                        <span>控制器</span>
-                        <em className="btn">重置</em>
-                    </div>
-                    <div className="canvas">
-                        <span className="contr c1"></span>
-                        <span className="contr c2"></span>
-                        <span className="contr c3"></span>
-                        <span className="contr c4"></span>
-                        <canvas id="canvas" width="800" height="537"></canvas>
-                    </div>
-                </div>
-
+  render() {
+    return (
+      <div className="App">
+        <div className="sidebar">
+          <div className="output">
+            <span>cubic-bezier(0.5, 0.5, 0.5, 0.5)</span>
+            <em className="btn">复制</em>
+          </div>
+          <div className="preset">
+            <div className="title">
+              <span>预设</span>
+              <em className="btn">复制</em>
             </div>
-        );
-    }
+            <ul className="preset-ul">
+              <li className="preset-item"></li>
+              <li className="preset-item"></li>
+              <li className="preset-item"></li>
+              <li className="preset-item"></li>
+              <li className="preset-item"></li>
+              <li className="preset-item"></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="stage">
+          <div className="title">
+            <span>控制器</span>
+            <em className="btn">重置</em>
+          </div>
+          <div className="canvas-w">
+            <div className="canvas" ref={this.canvasRef}>
+              <span className="contr c1" contr-type="c1"></span>
+              <span className="contr c2" contr-type="c2"></span>
+              <span className="contr c3" contr-type="c3"></span>
+              <span className="contr c4" contr-type="c4"></span>
+              <canvas id="canvas" width="500" height="500"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
